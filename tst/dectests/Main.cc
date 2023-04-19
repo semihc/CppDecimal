cfa150f058e440b2418ecc9c53c91ea140a4f48b
semihc <semih.cemiloglu@gmail.com>
2023-04-19 21:44:20 +1000

    First attempt at using Abseil-Cpp's flag and log library usage.

--- tst/dectests/Main.cc
+++ tst/dectests/Main.cc
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
+#include "absl/base/log_severity.h"
 #include "absl/flags/parse.h"
+#include "absl/log/log.h"
+#include "absl/log/initialize.h"
+#include "absl/log/globals.h"
+#include "absl/log/flags.h"
 
 int main(int argc, char **argv) 
 {
   auto remvec = absl::ParseCommandLine(argc, argv);
+  absl::InitializeLog();
+  absl::SetStderrThreshold(absl::LogSeverity::kInfo);
+  //VLOG(1) << "Before InitGoogleTest()";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
--- tst/dectests/dectest_t.cc
+++ tst/dectests/dectest_t.cc
@@ -1,19 +1,32 @@
 #include <decimal>
-#include <gtest/gtest.h>
 #include <cstdio>
 #include <string>
+#include "gtest/gtest.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/declare.h"
 
 using namespace std;
 
 int testVisitDirectory();
 int testProcessDecTestFile();
 
+ABSL_DECLARE_FLAG(std::string, dectests_dir);
+
+
+TEST(decTests, Directory) {
+ 
+  int rv = 0;
+  if( ! absl::GetFlag(FLAGS_dectests_dir).empty() ) {
+    // dectests_dir was set on the command line, thus visit and test
+    rv = testVisitDirectory();
+  }
+  ASSERT_TRUE(0 == rv);
+}
+
+
 TEST(decTests, Basic) {
  
-  int rv = testVisitDirectory();
-  ASSERT_TRUE(0 == rv);
-
-  rv = testProcessDecTestFile();
+  int rv = testProcessDecTestFile();
   ASSERT_TRUE(0 == rv);
 
 }
